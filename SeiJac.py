5cef7e54d4f9c4a809abc2ef4476a203a89abf1f
Daniel-Koz <33267845+Daniel-Koz@users.noreply.github.com>
2018-01-27 19:06:58 -0300

    Jacobi & Seidel v0.1

---
+++ SeiJac.py
@@ -0,0 +1,95 @@
+def Seidel(A,b,x,dr):
+    continua = 1
+    converge = 1
+    while(continua == 1 & converge == 1):
+        xTmp = x[:]
+        for i in range(0,len(A)):
+            aux = 0
+            for j in range(0,len(A)):
+                if (i != j):
+                    aux += (A[i][j]*x[j])
+                    x[i] = (b[i] - aux)/A[i][i]
+        continua = testa(xTmp,x,dr)
+        converge = conv(xTmp,x)
+    return x
+
+def Jacobi(A,b,x,dr):
+    continua = 1
+    converge = 1
+    while(continua == 1 & converge == 1):
+        xTmp = x[:]
+        for i in range(0,len(A)):
+            aux = 0
+            for j in range(0,len(A)):
+                if (i != j):
+                    aux += (A[i][j]*xTmp[j])
+                    x[i] = (b[i] - aux)/A[i][i]
+        continua = testa(xTmp,x,dr)
+        converge = conv(xTmp,x)
+    return x
+
+def testa(r1,r2,dr):
+    result = 0
+    print("\nNo vetor resposta\n",r2)
+    for i in range(0,len(r1)):
+        print("Desvio x",(i+1),"=",abs(r1[i] - r2[i]))
+        if (abs(r1[i] - r2[i]) >= dr ):
+            result = 1
+    return result
+
+def conv(r1,r2):
+    result = 1
+    for i in range(0,len(r1)):
+        if (abs(r1[i] - r2[i]) > 9999):
+            result = 0
+            print("Desvio muito grande, sistema não converge.")
+            break
+    return result
+
+def escolha(A,b,x,dr):
+    validade = 1
+    swi = input("1: Jacobi\n2: Seidel\n:")
+    if (swi == "1"):
+        print("Resposta:",Jacobi(A,b,x,dr))
+    elif (swi == "2"):
+        print("Resposta:",Seidel(A,b,x,dr))
+    else:
+        print("Opção inválida, retornando.")
+        validade = 0
+    return validade
+
+def mainJS():
+    print("Iniciando Jacobi e Seidel")
+    varQ = 1
+    while(varQ >= 1):
+        esco = 0
+        varQ = int(input("Quantas variáveis? (0 para ancerrar):"))
+        if(varQ >= 1):
+            matX = [x[:] for x in [[0.0]*(varQ+1)]*varQ]
+            
+            for i in range(varQ):
+                for j in range(varQ+1):
+                    print("Sistema:",matX)
+                    print("Linha:",i+1,"Coluna",j+1,":")
+                    matX[i][j] = float(input())
+            
+            res = [0.0]*varQ
+            vet = [0.0]*varQ
+            mat = [x[:] for x in [[0.0]*(varQ)]*varQ]
+            
+            for i in range(0,varQ):
+                vet[i] = matX[i][varQ]
+                for j in range(0,varQ):
+                    mat[i][j] = matX[i][j]
+            
+            desV = float(input("dr máximo:"))
+
+            print("Sistema:",matX)
+            
+            while(esco == 0):
+                print("Resolver Sistema utilizando qual metodo?")
+                esco = escolha(mat,vet,res,desV)
+        else:
+            print("Encerrando Jacobi e Seidel")
+
+mainJS()
